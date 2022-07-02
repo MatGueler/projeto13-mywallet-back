36014b9c6c94cd8c55567d691cafa53ce0db22fa
MattGueler <mateus.gueler@gmail.com>
2022-07-01 22:52:05 -0300

    Recebe as informações de saída

--- src/server.js
+++ src/server.js
@@ -132,4 +132,29 @@ server.post('/entrada', async (req, res) => {
 
 })
 
+// Saida
+server.post('/saida', async (req, res) => {
+
+    const id = ObjectId("62bde16b8a7194f30bc83691")
+
+    const date = dayjs().format('DD/MM')
+
+    const { price, description, type } = req.body
+
+    try {
+        const statement = await db.collection("statement").insertOne({
+            id,
+            date,
+            description,
+            price,
+            type
+        })
+
+        res.status(200).send(statement)
+    } catch {
+        res.sendStatus(500)
+    }
+
+})
+
 server.listen(5000)
